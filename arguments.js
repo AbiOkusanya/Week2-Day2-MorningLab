527f982ddb56b0f12a222ff083f6280a85939023
AbiOkusanya <73357072+AbiOkusanya@users.noreply.github.com>
2020-11-25 00:22:45 -0500

    24nov morning lab

---
+++ arguments.js
@@ -0,0 +1,68 @@
+// Biggest = findLargerOf( 5, 389);
+
+// function findLargerof (){
+//     const i = 5;
+//     const j = ;
+//     // const max = -Infinity;
+//     for (i = 0; i <arguments.length; i++) {
+//      if (arguments [i] > max) {
+//     max = arguments [i];
+//     } else if{
+//         (arguments [j] > max) {
+//             max = arguments [i];  
+//     }
+// }
+// return max;
+// }
+
+//Write a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. 
+//Use the if-then-else construct available in Javascript. Answer line 20 to 29
+// function findLargestnum(i, j) {
+//    const i = firstnum;
+//    const j = secondNum;
+   
+// if (i>j) {
+//     console.log (i);
+//     } else console.log (j);
+   
+//     }
+// findLargestnum( 7, 389)
+//
+//Write a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+//Answer lines 33 to 38
+// function findLargerOf3nums (a,b,c){
+//      if (a > b && a > c) console.log (a);
+//          if (b > a && b > c) console.log (b);
+//           if (c >a && c && b) console.log (c);
+// }
+// findLargerOf3nums (11, 78,456)
+//
+//Write a function isCharacterAVowel that takes a character (string of 1) and returns true if it is a 
+//vowel, false otherwise. Not working. Last attempt at the code is lines 42 -51
+// function isCharacterAVowel (x) {
+//     if (letter === A) return (true);
+//     if (letter === E) return (true);
+//     if (letter === I) return (true);
+//     if (letter === O) return (true);
+//     if (letter === U) return (true);
+// } else{
+//     return (false),
+// }
+// isCharacterAVowel (z) 
+
+
+
+//Write a function charCount that takes a string and returns the length of the string. lines 56 to 59
+function countChars () {
+    const str = "one day"; 
+    const n = str.length;
+}
+   
+//function to vowelCount a string and character check of 10 or less
+function vowelCount (string) {
+    let str = "You had me at hello";
+let vowels = str.match(/[aeiou]/g).length;
+    if (String.length > 10)
+// 
+console.log (`This has more than 10 characters${vowels}`); 
+}
